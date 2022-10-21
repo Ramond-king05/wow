ed8e0c1b5b7a78d847396e6cd7168b3e0fa7d7f8
Fasasi Abdulrahman Temitope <106965053+Ramond-king05@users.noreply.github.com>
2022-10-21 11:28:47 +0100

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.title("RAMOND-KING APPLICATION")
+st.title("FACEBOOK")
+st.text_input("Mobile number or email abi you don't ham nhi")
+st.text_input("Oya Enter Your Password KiaKia")
+st.button("Log in")
+st.balloons()
+st.write("Olodo you don forget your password?")
+st.button("click here")
+st.write("----------------------or------------------")
+st.button("Create new account")
+st.button("English(UK)")
+st.button("Hausa")
+st.button("Francais(France)")
+st.button("Yoruba ijinle")
+st.button("Arabic")
+st.subheader("About")
+
+
+
+st.text("FASASI ABDULRAHMAN T.")
+st.success("AN EXPERIENCED A.I/M.L DEVELOPER AND A DATA ANALYST ")
+ 
+st.markdown("easy and simple to use...")
+
+
+st.write("for more info contact us on: 09017152461")
+
+
